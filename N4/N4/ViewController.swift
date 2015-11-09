e2444ec47ac76edfa2de0b271e7b5fb42f38928f
Will <bigwillgluck@gmail.com>
2015-11-09 14:45:07 -0200

    Initial

---
+++ N4/N4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  N4
+//
+//  Created by Teclógica Serviços em Informática LTDA on 09/11/15.
+//  Copyright © 2015 FURB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
