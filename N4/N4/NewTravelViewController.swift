0b6f89be38da990b4d702e2049382531b9b386c6
William Maurício Glück <william.gluck@teclogica.com.br>
2015-11-18 17:32:59 -0200

    EIEIEIEI

---
+++ N4/N4/NewTravelViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewTravelViewController.swift
+//  N4
+//
+//  Created by Teclógica Serviços em Informática LTDA on 18/11/15.
+//  Copyright © 2015 FURB. All rights reserved.
+//
+
+import UIKit
+
+class NewTravelViewController: UIViewController {
+
+    @IBOutlet weak var travelName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ N4/N4/TravelTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TravelTableViewCell.swift
+//  N4
+//
+//  Created by Teclógica Serviços em Informática LTDA on 18/11/15.
+//  Copyright © 2015 FURB. All rights reserved.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
